6cf4404687a95733ff1ac51dd1725588ccc8f946
MSOLEH <leimportant@gmail.com>
2017-05-16 10:38:50 +0700

    add

---
+++ config/pdf.php
@@ -0,0 +1,21 @@
+<?php
+return [
+	'mode'                 => '',
+	'format'               => 'A4',
+	'default_font_size'    => '12',
+	'default_font'         => 'times',
+	'margin_left'          => 30,
+	'margin_right'         => 28,
+	'margin_top'           => 27,
+	'margin_bottom'        => 25,
+	'margin_header'        => 0,
+	'margin_footer'        => 0,
+	'orientation'          => 'P',
+	'title'                => 'Laravel mPDF',
+	'author'               => '',
+	'watermark'            => '',
+	'show_watermark'       => false,
+	'watermark_font'       => 'sans-serif',
+	'display_mode'         => 'fullpage',
+	'watermark_text_alpha' => 0.1
+];
